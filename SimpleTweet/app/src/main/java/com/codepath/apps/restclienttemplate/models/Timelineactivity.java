7dec4698c77993d4a7a2016712518d47778c831a
David Serrano <davidserrano@Davids-iMac.attlocal.net>
2021-02-14 11:34:01 -0800

    set up tweets to display properly

--- SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/Timelineactivity.java
+++ SimpleTweet/app/src/main/java/com/codepath/apps/restclienttemplate/models/Timelineactivity.java
@@ -1,20 +1,32 @@
 package com.codepath.apps.restclienttemplate.models;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.apps.restclienttemplate.R;
+import com.codepath.apps.restclienttemplate.TweetsAdapter;
 import com.codepath.apps.restclienttemplate.TwitterApp;
 import com.codepath.apps.restclienttemplate.TwitterClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Headers;
 
 public class Timelineactivity extends AppCompatActivity {
     public static final String TAG = "TimelineActivity";
     TwitterClient client;
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetsAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,14 @@ public class Timelineactivity extends AppCompatActivity {
         setContentView(R.layout.activity_timeline);
 
         client = TwitterApp.getRestClient(this);
+        // Find the recycler view
+        rvTweets = findViewById(R.id.rvTweets);
+        // init the list of tweets & adapter
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        // recycler view setup: layout manager & adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
         populateHomeTimeline();
     }
 
@@ -30,6 +50,13 @@ public class Timelineactivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.i(TAG, "onSuccess!");
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    tweets.addAll(Tweet.fromJsonArray(jsonArray));
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.i(TAG, "Json Exception", e);
+                }
             }
 
             @Override
